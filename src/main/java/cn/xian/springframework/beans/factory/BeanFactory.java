348887d0b571a391fde3d541decfcaff3cb1933f
lishixian <lisx@dataexa.com>
2019-10-15 19:58:26 +0800

    初次导入

--- src/main/java/cn/xian/springframework/beans/factory/BeanFactory.java
+++ src/main/java/cn/xian/springframework/beans/factory/BeanFactory.java
@@ -1,6 +1,6 @@
 package cn.xian.springframework.beans.factory;
 
-/**
+/**123
  * @author lishixian
  * @date 2019/10/15 下午7:45
  */
---
+++ src/main/java/cn/xian/springframework/beans/factory/config/BeanDefinition.java
@@ -0,0 +1,8 @@
+package cn.xian.springframework.beans.factory.config;
+
+/**
+ * @author lishixian
+ * @date 2019/10/15 下午7:58
+ */
+public class BeanDefinition {
+}
